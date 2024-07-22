8486860acded4b12d70d6186f16cd0beca6cc20a
Ievgen Kuznietsov <ievgen.contact@gmail.com>
2024-07-23 00:23:13 +0100

    v0.10 - JSON temperature list

--- MatoSwim/ContentView.swift
+++ MatoSwim/ContentView.swift
@@ -8,54 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var seaTemperature: Double = 0.0
     @StateObject private var webViewModel = WebViewModel()
-
+    
     var body: some View {
         VStack {
             Text("Water temperature in Matosinhos")
                 .font(.title)
                 .padding()
-
-            Text("\(seaTemperature, specifier: "%.1f")°C")
-                .font(.system(size: 60))
-                .fontWeight(.bold)
-
-            Spacer()
+            
             if let temperature = webViewModel.waterTemperature {
                 Text("\(temperature)°C")
                     .font(.largeTitle)
                     .fontWeight(.bold)
             } else {
-                ProgressView()
+                Text("N/A")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
             }
+            
+            // Only show "Last updated" if webViewModel.lastUpdated is not nil
+            if let lastUpdated = webViewModel.lastUpdated {
+                Text("Last updated: \(lastUpdated)")
+                    .font(.subheadline)
+                    .padding(.top)
+            }
+            
+            Button(action: {
+                webViewModel.checkTemperature()
+            }) {
+                Text("Refresh")
+            }
+            .padding(.top)
         }
-        .padding()
         .onAppear {
-            fetchSeaTemperature()
+            webViewModel.checkTemperature()
         }
-    }
-
-    func fetchSeaTemperature() {
-        guard let url = URL(string: "https://beachcam.meo.pt/livecams/praia-de-matosinhos/") else {
-            return
-        }
-
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            if let data = data, let html = String(data: data, encoding: .utf8) {
-                if let startIndex = html.range(of: "Temp. do mar")?.upperBound,
-                   let endIndex = html.range(of: "º", range: startIndex..<html.endIndex)?.lowerBound {
-                    let temperatureString = String(html[startIndex..<endIndex])
-                    if let temperature = Double(temperatureString) {
-                        DispatchQueue.main.async {
-                            seaTemperature = temperature
-                        }
-                    }
-                }
-            }
-            webViewModel.loadWebsite()
-        }
-        task.resume()
     }
 }
 
--- MatoSwim/WebViewModel.swift
+++ MatoSwim/WebViewModel.swift
@@ -7,36 +7,88 @@
 
 import WebKit
 import SwiftSoup
+import Foundation
 
 class WebViewModel: NSObject, ObservableObject {
     private let webView: WKWebView
     @Published var waterTemperature: String?
-
+    @Published var lastUpdated: String?
+    private var temperatureLog: [String] = []
+    
     override init() {
         let config = WKWebViewConfiguration()
         webView = WKWebView(frame: .zero, configuration: config)
         super.init()
         webView.navigationDelegate = self
-    }
+        
+        // Load temperature every 15 minutes in the background
+        Timer.scheduledTimer(withTimeInterval: 900, repeats: true) { [weak self] _ in
+            self?.checkTemperature()
+        }
 
-    func loadWebsite() {
+        // Load initial temperature from JSON file
+        fetchInitialTemperature()
+    }
+    
+    private func fetchInitialTemperature() {
+        if let path = Bundle.main.path(forResource: "matosinhos_water_temperatures", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+                if let jsonResult = jsonResult as? Dictionary<String, AnyObject>,
+                   let temperatures = jsonResult["temperatures"] as? [String: Double] {
+                    
+                    let dateFormatter = DateFormatter()
+                    dateFormatter.dateFormat = "MM-dd"
+                    let currentDate = dateFormatter.string(from: Date())
+                    
+                    if let tempValue = temperatures[currentDate] {
+                        let temperatureString = String(format: "%.1f", tempValue)
+                        DispatchQueue.main.async {
+                            self.waterTemperature = temperatureString
+                            // Don't set lastUpdated here
+                        }
+                    }
+                }
+            } catch {
+                print("Error reading or parsing matosinhos_water_temperatures.json: \(error)")
+            }
+        }
+    }
+    
+    func checkTemperature() {
         guard let url = URL(string: "https://beachcam.meo.pt/livecams/praia-de-matosinhos/") else { return }
         webView.load(URLRequest(url: url))
     }
-
+    
     private func parseHTML(_ html: String) {
         do {
             let document: Document = try SwiftSoup.parse(html)
-            let temperatureElement = try document.select("div.liveCamsDetailAside__weather-col-inside p").first()
+            let temperatureElement = try document.select("li.liveCamsHeader__infoList-item:contains(Temp. do mar) p").first()
             if let tempText = try temperatureElement?.text() {
-                let temperature = tempText.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
+                let temperatureInt = Int(tempText.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()) ?? 0
+                let temperatureFloat = Float(temperatureInt) / 10.0
                 DispatchQueue.main.async {
-                    self.waterTemperature = temperature
+                    let newTemperature = String(format: "%.1f", temperatureFloat)
+                    // Only update if the new temperature is different
+                    if newTemperature != self.waterTemperature {
+                        self.waterTemperature = newTemperature
+                        self.lastUpdated = self.getCurrentTime()
+                        self.temperatureLog.append(newTemperature)
+                    }
                 }
             }
         } catch {
-            print("Error parsing HTML: \(error)")
+            DispatchQueue.main.async {
+                print("Error parsing HTML: \(error)")
+            }
         }
+    }
+    
+    private func getCurrentTime() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm:ss"
+        return dateFormatter.string(from: Date())
     }
 }
 
