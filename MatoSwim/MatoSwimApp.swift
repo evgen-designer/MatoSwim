7fd3778eadf20ba4c26100d1a865fd622414b37e
Ievgen Kuznietsov <ievgen.contact@gmail.com>
2024-07-22 23:55:01 +0100

    Initial Commit

---
+++ MatoSwim/MatoSwimApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatoSwimApp.swift
+//  MatoSwim
+//
+//  Created by Mac on 22/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MatoSwimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
