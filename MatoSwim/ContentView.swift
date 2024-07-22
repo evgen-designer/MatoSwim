7d7ef498460d35755acd73856fb5fd81b073f3ef
Ievgen Kuznietsov <ievgen.contact@gmail.com>
2024-07-22 23:58:32 +0100

    v0.01 - Matosinhos temperature

--- MatoSwim/ContentView.swift
+++ MatoSwim/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var seaTemperature: Double = 0.0
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Matosinhos sea temperature")
+                .font(.title)
+                .padding()
+
+            Text("\(seaTemperature, specifier: "%.1f")°C")
+                .font(.system(size: 60))
+                .fontWeight(.bold)
+
+            Spacer()
         }
         .padding()
+        .onAppear {
+            fetchSeaTemperature()
+        }
+    }
+
+    func fetchSeaTemperature() {
+        guard let url = URL(string: "https://beachcam.meo.pt/livecams/praia-de-matosinhos/") else {
+            return
+        }
+
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data, let html = String(data: data, encoding: .utf8) {
+                if let startIndex = html.range(of: "Temp. do mar")?.upperBound,
+                   let endIndex = html.range(of: "º", range: startIndex..<html.endIndex)?.lowerBound {
+                    let temperatureString = String(html[startIndex..<endIndex])
+                    if let temperature = Double(temperatureString) {
+                        DispatchQueue.main.async {
+                            seaTemperature = temperature
+                        }
+                    }
+                }
+            }
+        }
+        task.resume()
     }
 }
 
